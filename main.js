5c616e560d4577a042ba2bff018e13f590da38a4
Evelin <144942598+evelinAlbino@users.noreply.github.com>
2025-04-08 11:24:28 -0300

    nn

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2026-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-08-01 T00:00:00");
+const tempoObjetivo1 = new Date("2025-12-19T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-19T00:00:00");
+const tempoObjetivo3 = new Date("2026-02-08T00:00:00");
+const tempoObjetivo4 = new Date("2027-01-01 T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
